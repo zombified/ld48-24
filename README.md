eeca14de110a3374094b4127fb591bcab655f484
Joel Kleier <joel.kleier@gmail.com>
2012-08-25 12:03:00 -0500

    got all the mechanics basically working, need to tweak some values, create a start screen, and allow a player to enter high scores

---
+++ README.md
@@ -0,0 +1,3 @@
+Theme: EVOLUTION
+
+The goal of this game is to survive. To survive, you need to change to suit your environment.
--- main.lua
+++ main.lua
@@ -5,30 +5,71 @@ local maxblobs = 100;
 local blobspdmin = 40;
 local blobspdmax = 90;
 local blobradiusmin = 2;
-local blobradiusmax = 15;
+local blobradiusmax = 30;
 
-local playerradius = 10;
+local playerradius_start = 5
+local playerradius = playerradius_start;
 local playerhit = false;
 
 local playerColor = {255, 0, 0, 255};
 local enemyColor = {0, 255, 0, 255};
 local hitColor = {255, 0, 255};
+local negColor = {255, 255, 0};
+
+local growthAmount = 1;
+local shrinkAmount = 1;
+local decayAmount = 1;
+local decayRate = 2;
+local curDecay = 0;
+
+local starttime = nil;
+local endtime = nil;
+
+
+
+function resetBlob(blob, minx, maxx)
+    minx = minx or 830;
+    maxx = maxx or 900;
+
+    blob.x = math.random(minx, maxx);
+    blob.y = math.random(1, 600);
+    blob.r = math.random(blobradiusmin, blobradiusmax);
+    blob.spd = math.random(blobspdmin, blobspdmax);
+    blob.hit = false
+    blob.isneg = math.random(1, 4);
+end
+
+function reset()
+    playerradius = playerradius_start;
+    playerhit = false;
+    for i = 1, maxblobs do
+        resetBlob(blobs[i], 630, 1430);
+    end
+    starttime = love.timer.getTime();
+    endtime = nil;
+end
+
+
 
 
 function love.load()
     love.mouse.setVisible(false);
 
     for i = 1, maxblobs do
-        table.insert(blobs, {x=math.random(1, 800),
-                             y=math.random(1, 600),
-                             r=math.random(blobradiusmin, blobradiusmax),
-                             spd=math.random(blobspdmin, blobspdmax),
-                             hit=false});
+        blob = {x=nil, y=nil, r=nil, spd=nil, hit=false, isneg=nil};
+        resetBlob(blob, 630, 1430);
+        table.insert(blobs, blob);
     end
+
+    starttime = love.timer.getTime()
 end
 
 
 function love.update(dt)
+    if playerradius < 1 then
+        return;
+    end
+
     playerhit = false;
 
     local blobv;
@@ -38,23 +79,49 @@ function love.update(dt)
 
         -- update blob position
         blobs[i].x = blobs[i].x - blobs[i].spd * dt;
-        if blobs[i].x < -35 then
-            blobs[i].x = math.random(830, 900);
-            blobs[i].y = math.random(1, 600);
-            blobs[i].r = math.random(blobradiusmin, blobradiusmax);
-            blobs[i].spd = math.random(blobspdmin, blobspdmax);
+
+        -- move blob to beginning of flow if off screen or effectively dead
+        if blobs[i].x < -35 or blobs[i].r < 1 then
+            resetBlob(blobs[i]);
         end
 
         -- check for collision
         blobv = vector(blobs[i].x, blobs[i].y);
         if playerv:dist(blobv) <= blobs[i].r + playerradius then
             blobs[i].hit = true;
+
+            if blobs[i].r > playerradius or blobs[i].isneg == 1 then
+                -- shrink player
+                blobs[i].r = blobs[i].r + growthAmount;
+                playerradius = playerradius - shrinkAmount;
+            else
+                -- grow player
+                blobs[i].r = blobs[i].r - shrinkAmount;
+                playerradius = playerradius + growthAmount;
+            end
         end
+    end
+
+    curDecay = curDecay + dt;
+    if curDecay > decayRate then
+        curDecay = 0;
+        playerradius = playerradius - decayAmount;
+    end
+
+    if playerradius < 1 and endtime == nil then
+        endtime = love.timer.getTime();
     end
 end
 
 
 function love.draw()
+    if playerradius < 1 then
+        love.graphics.print("Shit, you died!", 350, 92);
+        love.graphics.print("But you did manage to live for " .. (endtime-starttime) .. "s!", 280, 124);
+        return;
+    end
+
+
     mousex = love.mouse.getX();
     mousey = love.mouse.getY();
     if not playerhit then
@@ -65,12 +132,16 @@ function love.draw()
     love.graphics.circle("fill", mousex, mousey, playerradius);
 
     for i = 1, maxblobs do
-        if not blobs[i].hit then
-            love.graphics.setColor(enemyColor);
-        else
-            love.graphics.setColor(hitColor);
+        if blobs[i].r >= 1 then
+            if blobs[i].hit then
+                love.graphics.setColor(hitColor);
+            elseif blobs[i].isneg == 1 then
+                love.graphics.setColor(negColor);
+            else
+                love.graphics.setColor(enemyColor);
+            end
+            love.graphics.circle("fill", blobs[i].x, blobs[i].y, blobs[i].r);
         end
-        love.graphics.circle("fill", blobs[i].x, blobs[i].y, blobs[i].r);
     end
 end
 
@@ -78,5 +149,7 @@ end
 function love.keyreleased(key)
     if key == "escape" then
         love.event.quit();
+    elseif key == "r" then
+        reset()
     end
 end
